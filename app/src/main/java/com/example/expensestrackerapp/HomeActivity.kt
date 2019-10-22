326b2505043d0dd07ab9d42b8b23b6047b6e2dc0
Beverly May Sellon Castillo <beverlymaycastillo@gmail.com>
2019-10-22 10:10:33 +0800

    fixed sign in with google

--- app/src/main/java/com/example/expensestrackerapp/HomeActivity.kt
+++ app/src/main/java/com/example/expensestrackerapp/HomeActivity.kt
@@ -1,12 +1,34 @@
 package com.example.expensestrackerapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+
+
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //
+        var txtLastname = findViewById(R.id.txtLastname) as TextView
+        var txtFirstname = findViewById(R.id.txtFirstname) as TextView
+        var txtDisplayName = findViewById(R.id.txtDisplayName) as TextView
+        var txtEmail = findViewById(R.id.txtEmail) as TextView
+        var txtID = findViewById(R.id.txtID) as TextView
+        var txtPhotoURL = findViewById(R.id.txtPhotoURL) as TextView
+
+        val acct = GoogleSignIn.getLastSignedInAccount(this)
+        if (acct != null) {
+            txtID.text = acct.id
+            txtDisplayName.text = acct.displayName
+            txtLastname.text = acct.familyName
+            txtFirstname.text = acct.givenName
+            txtEmail.text = acct.email
+
+        }
     }
 }
--- app/src/main/java/com/example/expensestrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/expensestrackerapp/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.expensestrackerapp
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+
+
+
 
 const val RC_SIGN_IN = 123
 
@@ -14,38 +22,72 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
             .build()
 
+        // Build a GoogleSignInClient with the options specified by gso.
         val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-        //get reference to all the views
-        var btnGoogleSignin = findViewById(R.id.btnSigninGoogle) as Button
-        var btnFacebookLogin = findViewById(R.id.btnLoginFacebook) as Button
+//        //get reference to all the views
+//        var btnGoogleSignin = findViewById(R.id.btnSigninGoogle) as Button
+//        var btnFacebookLogin = findViewById(R.id.btnLoginFacebook) as Button
 
-        btnGoogleSignin.setOnClickListener {
+        var btnGoogleSignIn = findViewById(R.id.sign_in_button) as SignInButton
+
+        btnGoogleSignIn.setOnClickListener {
             val signInIntent = mGoogleSignInClient.signInIntent
             startActivityForResult(signInIntent, RC_SIGN_IN)
         }
-//        val acct = GoogleSignIn.getLastSignedInAccount(this)
-//        if (acct != null) {
-//            btnSigninGoogle.visibility = View.GONE
-//        }
 
-//        btnGoogleSignin.setOnClickListener {
-//            val signInIntent = mGoogleSignInClient.signInIntent
-//            startActivityForResult(signInIntent, RC_SIGN_IN)
-//        }
-//
-//        btnFacebookLogin.setOnClickListener {
-//
-//        }
-
-
+        //this is getting the account details of the user
+        val acct = GoogleSignIn.getLastSignedInAccount(this)
+        if (acct != null) {
+            val personName = acct.displayName
+            val personGivenName = acct.givenName
+            val personFamilyName = acct.familyName
+            val personEmail = acct.email
+            val personId = acct.id
+            val personPhoto = acct.photoUrl
+        }
 
 
     }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+//            updateUI(account)
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+//            Log.w(FragmentActivity.TAG, "signInResult:failed code=" + e.statusCode)
+//            updateUI(null)
+
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
 
 
 }
